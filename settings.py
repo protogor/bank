538e4a6ac71b529d8b1571b5584c5ce6922db1f0
slava <protogorproject@gmail.com>
2017-04-19 20:31:04 +0300

    fix settings.py pep8

--- settings.py
+++ settings.py
@@ -1,41 +1,36 @@
 from datetime import datetime
 from database import db_session
 from flask import Flask, session, g, render_template
-from bank.models import User
+
+from utils import utils
+from bank.views import general
 
 
 app = Flask(__name__)
 
-
+# handler error 404
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
 
-
+# middleware
 @app.teardown_request
 def remove_db_session(exception):
     db_session.remove()
 
 
+@app.before_request
+def load_current_user():
+	pass
+
+# context processor
 @app.context_processor
 def date_context():
     return {'current_date_context': datetime.now()}
 
 
-@app.before_request
-def load_current_user():
-	pass
-	
-
-from bank.views import general
-
+# blueprint
 app.register_blueprint(general)
 
-
-from utils import utils
+# filter jinja
 app.jinja_env.filters['current_date'] = utils.get_current_date
-"""
-app.jinja_env.filters['dateformat'] = utils.format_date
-app.jinja_env.filters['timedeltaformat'] = utils.format_timedelta
-app.jinja_env.filters['displayopenid'] = utils.display_openid
-"""
